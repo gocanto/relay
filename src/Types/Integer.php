a8346aae19b8051df5fb5570826ec2a5f244f33e
Gustavo <gustavoocanto@gmail.com>
2020-04-03 16:48:33 +0800

    add the integer type

---
+++ src/Types/Integer.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+
+namespace Gocanto\Attributes\Types;
+
+use Gocanto\Attributes\AttributesException;
+use Gocanto\Attributes\Type;
+
+class Integer implements Type
+{
+    private int $value;
+
+    private function __construct()
+    {
+    }
+
+    /**
+     * @param $value
+     * @return Type
+     * @throws AttributesException
+     */
+    public static function make($value): Type
+    {
+        $integer = new static();
+        $integer->value = $integer->parse($value);
+
+        return $integer;
+    }
+
+    /**
+     * @param string|int|mixed $value
+     * @return int
+     * @throws AttributesException
+     */
+    private function parse($value): int
+    {
+        if (is_integer($value)) {
+            return $value;
+        }
+
+        if ($this->canBeInteger($value)) {
+            return (int) $value;
+        }
+
+        $ref = $this->isNotAllowed($value) ? '' : "[{$value}]";
+
+        throw new AttributesException("The given number {$ref} is invalid.");
+    }
+
+    private function canBeInteger($value): bool
+    {
+        if (!is_string($value) || $this->isNotAllowed($value)) {
+            return false;
+        }
+
+        $candidate = (int) $value;
+
+        return ((string) $candidate) === $value;
+    }
+
+    private function isNotAllowed($value): bool
+    {
+        return is_object($value) || is_callable($value);
+    }
+
+    public function get(): int
+    {
+        return $this->value;
+    }
+}
---
+++ tests/Types/IntegerTest.php
@@ -0,0 +1,119 @@
+<?php declare(strict_types=1);
+
+namespace Gocanto\Attributes\Tests\Types;
+
+use Gocanto\Attributes\AttributesException;
+use Gocanto\Attributes\Promoter;
+use Gocanto\Attributes\Tests\Stubs\Payload;
+use Gocanto\Attributes\Types\Integer;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+class IntegerTest extends TestCase
+{
+    /**
+     * @test
+     * @throws AttributesException
+     */
+    public function itHoldsValidIntegers()
+    {
+        $payload = new Payload([
+            'field-1' => 1,
+            'field-2' => -1,
+            'field-3' => 0,
+            'field-4' => 9807654,
+        ], [
+            'field-1' => Promoter::make(Integer::class),
+            'field-2' => Promoter::make(Integer::class),
+            'field-3' => Promoter::make(Integer::class),
+            'field-4' => Promoter::make(Integer::class),
+        ]);
+
+        $fieldOne = $payload->get('field-1');
+        $this->assertInstanceOf(Integer::class, $fieldOne);
+        $this->assertSame(1, $fieldOne->get());
+
+        $fieldTwo = $payload->get('field-2');
+        $this->assertInstanceOf(Integer::class, $fieldTwo);
+        $this->assertSame(-1, $fieldTwo->get());
+
+        $fieldThree = $payload->get('field-3');
+        $this->assertInstanceOf(Integer::class, $fieldThree);
+        $this->assertSame(0, $fieldThree->get());
+
+        $fieldFour = $payload->get('field-4');
+        $this->assertInstanceOf(Integer::class, $fieldFour);
+        $this->assertSame(9807654, $fieldFour->get());
+    }
+
+    /**
+     * @test
+     * @throws AttributesException
+     */
+    public function itProperlyParseValidStringContainingIntegers()
+    {
+        $payload = new Payload([
+            'field-1' => '1',
+            'field-2' => '-1',
+            'field-3' => '0',
+            'field-4' => '9807654',
+        ], [
+            'field-1' => Promoter::make(Integer::class),
+            'field-2' => Promoter::make(Integer::class),
+            'field-3' => Promoter::make(Integer::class),
+            'field-4' => Promoter::make(Integer::class),
+        ]);
+
+        $fieldOne = $payload->get('field-1');
+        $this->assertInstanceOf(Integer::class, $fieldOne);
+        $this->assertSame(1, $fieldOne->get());
+
+        $fieldTwo = $payload->get('field-2');
+        $this->assertInstanceOf(Integer::class, $fieldTwo);
+        $this->assertSame(-1, $fieldTwo->get());
+
+        $fieldThree = $payload->get('field-3');
+        $this->assertInstanceOf(Integer::class, $fieldThree);
+        $this->assertSame(0, $fieldThree->get());
+
+        $fieldFour = $payload->get('field-4');
+        $this->assertInstanceOf(Integer::class, $fieldFour);
+        $this->assertSame(9807654, $fieldFour->get());
+    }
+
+    /**
+     * @test
+     * @throws AttributesException
+     */
+    public function itGuardsAgainstInvalidIntegers()
+    {
+        $this->expectException(AttributesException::class);
+        $this->expectExceptionMessageMatches('/invalid/');
+
+        $payload = new Payload([
+            'field-1' => 'foo',
+        ], [
+            'field-1' => Promoter::make(Integer::class),
+        ]);
+
+        $payload->get('field-1');
+    }
+
+    /**
+     * @test
+     * @throws AttributesException
+     */
+    public function itGuardsAgainstNotAllowedValues()
+    {
+        $this->expectException(AttributesException::class);
+        $this->expectExceptionMessageMatches('/invalid/');
+
+        $payload = new Payload([
+            'field-1' => new stdClass(),
+        ], [
+            'field-1' => Promoter::make(Integer::class),
+        ]);
+
+        $payload->get('field-1');
+    }
+}
