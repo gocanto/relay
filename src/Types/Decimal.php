f628f46a3648b3a28a15ccd2ae0a08d899a96602
Gustavo <gustavoocanto@gmail.com>
2020-04-03 17:59:53 +0800

    add the decimal type

---
+++ src/Types/Decimal.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+
+namespace Gocanto\Attributes\Types;
+
+use Gocanto\Attributes\AttributesException;
+use Gocanto\Attributes\Type;
+
+class Decimal implements Type
+{
+    private float $value;
+
+    private function __construct()
+    {
+    }
+
+    /**
+     * @param $value
+     * @return Type
+     * @throws AttributesException
+     */
+    public static function make($value): Type
+    {
+        $decimal = new static();
+        $decimal->value = $decimal->parse($value);
+
+        return $decimal;
+    }
+
+    /**
+     * @param string|float|mixed $value
+     * @return float
+     * @throws AttributesException
+     */
+    private function parse($value): float
+    {
+        if (is_float($value) || is_integer($value)) {
+            return (float) $value;
+        }
+
+        if ($this->canBeDecimal($value)) {
+            return (float) $value;
+        }
+
+        $ref = $this->isNotAllowed($value) ? '' : "[{$value}]";
+
+        throw new AttributesException("The given number {$ref} is invalid.");
+    }
+
+    private function canBeDecimal($value): bool
+    {
+        if (!is_string($value) || $this->isNotAllowed($value)) {
+            return false;
+        }
+
+        $candidate = (float) $value;
+
+        return ((string) $candidate) === $value;
+    }
+
+    private function isNotAllowed($value): bool
+    {
+        return is_object($value) || is_callable($value);
+    }
+
+    public function get(): float
+    {
+        return $this->value;
+    }
+}
---
+++ tests/Types/DecimalTest.php
@@ -0,0 +1,119 @@
+<?php declare(strict_types=1);
+
+namespace Gocanto\Attributes\Tests\Types;
+
+use Gocanto\Attributes\AttributesException;
+use Gocanto\Attributes\Promoter;
+use Gocanto\Attributes\Tests\Stubs\Payload;
+use Gocanto\Attributes\Types\Decimal;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+class DecimalTest extends TestCase
+{
+    /**
+     * @test
+     * @throws AttributesException
+     */
+    public function itHoldsValidDecimals()
+    {
+        $payload = new Payload([
+            'field-1' => 1.1,
+            'field-2' => -1.1,
+            'field-3' => 0,
+            'field-4' => 9807.654,
+        ], [
+            'field-1' => Promoter::make(Decimal::class),
+            'field-2' => Promoter::make(Decimal::class),
+            'field-3' => Promoter::make(Decimal::class),
+            'field-4' => Promoter::make(Decimal::class),
+        ]);
+
+        $fieldOne = $payload->get('field-1');
+        $this->assertInstanceOf(Decimal::class, $fieldOne);
+        $this->assertSame(1.1, $fieldOne->get());
+
+        $fieldTwo = $payload->get('field-2');
+        $this->assertInstanceOf(Decimal::class, $fieldTwo);
+        $this->assertSame(-1.1, $fieldTwo->get());
+
+        $fieldThree = $payload->get('field-3');
+        $this->assertInstanceOf(Decimal::class, $fieldThree);
+        $this->assertSame(0.0, $fieldThree->get());
+
+        $fieldFour = $payload->get('field-4');
+        $this->assertInstanceOf(Decimal::class, $fieldFour);
+        $this->assertSame(9807.654, $fieldFour->get());
+    }
+
+    /**
+     * @test
+     * @throws AttributesException
+     */
+    public function itProperlyParseValidStringContainingDecimals()
+    {
+        $payload = new Payload([
+            'field-1' => '1.1',
+            'field-2' => '-1.1',
+            'field-3' => '0',
+            'field-4' => '9807.654',
+        ], [
+            'field-1' => Promoter::make(Decimal::class),
+            'field-2' => Promoter::make(Decimal::class),
+            'field-3' => Promoter::make(Decimal::class),
+            'field-4' => Promoter::make(Decimal::class),
+        ]);
+
+        $fieldOne = $payload->get('field-1');
+        $this->assertInstanceOf(Decimal::class, $fieldOne);
+        $this->assertSame(1.1, $fieldOne->get());
+
+        $fieldTwo = $payload->get('field-2');
+        $this->assertInstanceOf(Decimal::class, $fieldTwo);
+        $this->assertSame(-1.1, $fieldTwo->get());
+
+        $fieldThree = $payload->get('field-3');
+        $this->assertInstanceOf(Decimal::class, $fieldThree);
+        $this->assertSame(0.0, $fieldThree->get());
+
+        $fieldFour = $payload->get('field-4');
+        $this->assertInstanceOf(Decimal::class, $fieldFour);
+        $this->assertSame(9807.654, $fieldFour->get());
+    }
+
+    /**
+     * @test
+     * @throws AttributesException
+     */
+    public function itGuardsAgainstInvalidIntegers()
+    {
+        $this->expectException(AttributesException::class);
+        $this->expectExceptionMessageMatches('/invalid/');
+
+        $payload = new Payload([
+            'field-1' => 'foo',
+        ], [
+            'field-1' => Promoter::make(Decimal::class),
+        ]);
+
+        $payload->get('field-1');
+    }
+
+    /**
+     * @test
+     * @throws AttributesException
+     */
+    public function itGuardsAgainstNotAllowedValues()
+    {
+        $this->expectException(AttributesException::class);
+        $this->expectExceptionMessageMatches('/invalid/');
+
+        $payload = new Payload([
+            'field-1' => new stdClass(),
+        ], [
+            'field-1' => Promoter::make(Decimal::class),
+        ]);
+
+        $payload->get('field-1');
+    }
+}
